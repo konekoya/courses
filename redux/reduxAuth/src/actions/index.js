61b57379faf96347d0c779ed807d6f0977302ed2
Konekoya <tank158r5155@yahoo.com.tw>
2017-05-25 09:12:15 +0800

    update

--- redux/reduxAuth/src/actions/index.js
+++ redux/reduxAuth/src/actions/index.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/signin`, {email, password});
+    axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+        console.log(response);
+        dispatch({ type: AUTH_USER });
+
+        localStorage.setItem('token', response.data.token);
+
+        browserHistory.push('/feature');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
   };
 }
