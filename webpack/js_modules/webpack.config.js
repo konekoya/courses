dbf3305b89c1fc3e412c540dd707f2fba191764b
Konekoya <tank158r5155@yahoo.com.tw>
2017-06-02 18:20:04 +0800

    update

--- webpack/js_modules/webpack.config.js
+++ webpack/js_modules/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -9,15 +10,32 @@ const config = {
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/,
+        use: 'babel-loader'
       },
       {
-        use: ['style-loader', 'css-loader'],
-        test: /\.css$/
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4000
+            }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
