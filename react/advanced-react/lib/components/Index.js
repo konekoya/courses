9c86cd75e30280bb6a671ca59880d8c19d313996
Konekoya <tank158r5155@yahoo.com.tw>
2017-08-24 09:54:33 +0800

    Finish module 1

--- react/advanced-react/lib/components/Index.js
+++ react/advanced-react/lib/components/Index.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  state = {
-    anwser: 42,
-  };
-
-  asyncFunc = () => {
-    return Promise.resolve(27);
-  };
-  async componentDidMount() {
-    this.setState({
-      anwser: await this.asyncFunc(),
-    });
-  }
-
-  render() {
-    return (
-      <h2>
-        Hello React -- {this.state.anwser}{' '}
-      </h2>
-    );
-  }
-}
-
-export default App;
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
