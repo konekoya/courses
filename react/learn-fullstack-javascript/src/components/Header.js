fa4ea115a322033b55641c5668e84642a753bb89
Konekoya <tank158r5155@yahoo.com.tw>
2017-11-03 09:52:37 +0800

    React and test data

---
+++ react/learn-fullstack-javascript/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = ({ message }) => {
+  return <h1>{message}</h1>;
+};
+
+Header.propTypes = {
+  message: PropTypes.string,
+};
+
+export default Header;
