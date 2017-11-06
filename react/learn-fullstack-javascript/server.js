fc99b36c967699346c087889653903f224e248d2
Konekoya <tank158r5155@yahoo.com.tw>
2017-11-06 09:30:50 +0800

    More react components

--- react/learn-fullstack-javascript/server.js
+++ react/learn-fullstack-javascript/server.js
@@ -1,8 +1,17 @@
 import config from './config';
 import apiRouter from './api';
-
+import sassMiddleware from 'node-sass-middleware';
 import express from 'express';
+import path from 'path';
+
 const server = express();
+
+server.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+  }),
+);
 
 server.set('view engine', 'ejs');
 
--- react/learn-fullstack-javascript/src/components/App.js
+++ react/learn-fullstack-javascript/src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests!!" />
-    </div>
-  );
-};
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+import data from '../testData';
+
+class App extends React.Component {
+  state = {
+    contests: [],
+  };
+
+  componentWillMount() {
+    this.setState({ contests: data.contests });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message="Naming Contests!!" />
+        <div>
+          {this.state.contests.map(contest => {
+            return <ContestPreview key={contest.id} {...contest} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
