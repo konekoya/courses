d28e2fb94bc97e01202e4bf0dedbe58cf4632bd0
Konekoya <tank158r5155@yahoo.com.tw>
2018-02-02 06:03:55 +0800

    Finish exception handling module

--- python/exceptional.py
+++ python/exceptional.py
@@ -1,11 +1,13 @@
 '''A module for demonstrating exceptions.'''
 
+import sys
+
 def convert(s):
     '''Convert to an integer.'''
-    x = -1
     try:
-        x = int(s)
-        print('Convertion succeeded! x =', x)
-    except (ValueError, TypeError):
-        print('Convertion failed')
-    return x
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print('Convertion error: {}'\
+          .format(str(e)),
+          file=sys.stderr)
+    return -1
