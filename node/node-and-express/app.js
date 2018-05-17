b5aa2fa96ebea6a00b0b57f419ff95a536ff632e
Konekoya <tank158r5155@yahoo.com.tw>
2018-05-17 10:12:39 +0800

    Debug

--- node/node-and-express/app.js
+++ node/node-and-express/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+const port = 5000;
+
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/boostrap/dist/js')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
+);
 
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(5000, () => {
-  console.log('listening on port 5000');
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
