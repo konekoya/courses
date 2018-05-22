5b07c268fb1083b1b997350e3b3a5c886209e75e
Konekoya <tank158r5155@yahoo.com.tw>
2018-05-22 09:58:45 +0800

    Update

--- node/node-and-express/app.js
+++ node/node-and-express/app.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const bookRouter = express.Router();
 
 app.use(morgan('combined'));
 
@@ -16,15 +17,77 @@ app.use(
 );
 app.use(
   '/js',
-  express.static(path.join(__dirname, '/node_modules/boostrap/dist/js')),
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
 );
 app.use(
   '/js',
   express.static(path.join(__dirname, '/node_modules/jquery/dist')),
 );
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')),
+);
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false,
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false,
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false,
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false,
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false,
+  },
+];
+bookRouter.route('/').get((req, res) => {
+  res.render('books', {
+    title: 'Library',
+    nav: [
+      { link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' },
+    ],
+    books,
+  });
+});
+
+bookRouter.route('/single').get((req, res) => {
+  res.send('hello single book');
+});
+
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', {
+    title: 'Library',
+    nav: [
+      { link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' },
+    ],
+  });
 });
 
 app.listen(port, () => {
