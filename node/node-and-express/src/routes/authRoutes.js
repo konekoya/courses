07ebd9a6ec288cf3258e524e50734bf3f683751a
Konekoya <tank158r5155@yahoo.com.tw>
2018-05-30 09:56:54 +0800

    Auth

---
+++ node/node-and-express/src/routes/authRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:authRoutes');
+
+const authRouter = express.Router();
+
+function router(params) {
+  authRouter.route('/signUp').post((req, res) => {
+    debug(req.body);
+    req.login(req.body, () => {
+      res.redirect('/auth/profile');
+    });
+  });
+
+  authRouter.route('/profile').get((req, res) => {
+    res.json(req.user);
+  });
+
+  return authRouter;
+}
+
+module.exports = router;
