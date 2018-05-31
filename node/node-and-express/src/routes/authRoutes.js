d188ae779f6b1ff3075bc09ef0c10756027cc21c
Konekoya <tank158r5155@yahoo.com.tw>
2018-05-31 09:58:46 +0800

    Update

--- node/node-and-express/src/routes/authRoutes.js
+++ node/node-and-express/src/routes/authRoutes.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const passport = require('passport');
 const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
 
-function router(params) {
+function router(nav) {
   authRouter.route('/signUp').post((req, res) => {
-    debug(req.body);
-    req.login(req.body, () => {
-      res.redirect('/auth/profile');
-    });
+    const { username, password } = req.body;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function addUser() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connneted correctly to server');
+
+        const db = client.db(dbName);
+        const col = db.collection('users');
+        const user = { username, password };
+        const results = await col.insertOne(user);
+
+        req.login(results.ops[0], () => {
+          res.redirect('/auth/profile');
+        });
+      } catch (err) {
+        debug(err);
+      }
+    }());
   });
+
+  authRouter
+    .route('/signin')
+    .get((req, res) => {
+      res.render('signin', {
+        nav,
+        title: 'Sign In',
+      });
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/',
+    }));
 
   authRouter.route('/profile').get((req, res) => {
     res.json(req.user);
