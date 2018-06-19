b74fa3ed1518d082dcfc94f24f229591c4d1baf7
Konekoya <tank158r5155@yahoo.com.tw>
2018-06-19 10:04:39 +0800

    New course -- graphql

---
+++ graph-ql/build-graphql-apis-with-node-and-mongodb/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+const PORT = 3000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  }),
+);
+
+app.get('/', (req, res) => {
+  return res.json({
+    msg: 'hello graphql',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
