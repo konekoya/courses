4aacdc5fb1094764e8fa8d5bdec89d1dd7807c3f
Konekoya <tank158r5155@yahoo.com.tw>
2018-07-09 09:52:32 +0800

    Use pug

--- node/node-crash-course/routes/index.js
+++ node/node-crash-course/routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello', { title: 'I love food' });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = req.params.name.reverse().join;
+  console.log(req.params);
 });
 
 module.exports = router;
