aee279a11581d76fb5fc0a339843d23904f9dac5
Konekoya <tank158r5155@yahoo.com.tw>
2018-09-11 09:32:57 +0800

    Add review

--- node/node-crash-course/routes/index.js
+++ node/node-crash-course/routes/index.js
@@ -56,6 +56,11 @@ router.post(
 );
 
 router.get('/map', storeController.mapPage);
+router.get(
+  '/hearts',
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts),
+);
 
 // API
 
