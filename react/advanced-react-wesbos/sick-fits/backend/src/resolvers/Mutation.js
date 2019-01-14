b092ac93b8362ad60faa1151d36de9c2fd2133d0
Konekoya <tank158r5155@yahoo.com.tw>
2019-01-14 09:29:10 +0800

    Query and mutation

--- react/advanced-react-wesbos/sick-fits/backend/src/resolvers/Mutation.js
+++ react/advanced-react-wesbos/sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // Create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+
+    console.log(args);
+  }
+};
 
 module.exports = mutations;
