983f9fa5042329705e4aa6b23fbe2153ebaf2191
Konekoya <tank158r5155@yahoo.com.tw>
2019-02-12 09:37:20 +0800

    More on promise

--- js/egghead-promise/index.js
+++ js/egghead-promise/index.js
@@ -1,14 +1,14 @@
 const output = document.getElementById("output");
+const spinner = document.getElementById("spinner");
 
-const API_URL = "https://starwars.egghead.training/films";
-
-output.innerText = "Loading...";
+const API_URL = "https://starwars.egghead.training/film";
 
 fetch(API_URL)
   .then(res => {
     if (!res.ok) {
       throw Error("Unsuccessful request");
     }
+
     return res.json().then(films => {
       const result = getFilms(films);
       output.innerText = result;
@@ -17,6 +17,9 @@ fetch(API_URL)
   .catch(error => {
     console.log(error);
     output.innerText = "Sorry, but something went wrong!";
+  })
+  .finally(() => {
+    spinner.remove();
   });
 
 const getFilms = films => {
