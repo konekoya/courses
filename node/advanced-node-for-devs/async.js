36a1ad062132d27f85a5c74fe7abac312f12b97c
Konekoya <tank158r5155@yahoo.com.tw>
2019-04-30 08:39:10 +0800

    Libuv

---
+++ node/advanced-node-for-devs/async.js
@@ -0,0 +1,64 @@
+const http = require("http");
+
+const start = Date.now();
+
+function doRequest() {
+  http
+    .request("http://www.google.com", res => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log(Date.now() - start);
+      });
+    })
+    .end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
