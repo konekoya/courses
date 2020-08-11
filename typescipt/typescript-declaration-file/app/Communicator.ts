e8a52e6d8f6586c58247d015aac7c158579d7f3e
Joshua <konekoya@gmail.com>
2020-08-11 09:41:13 +0800

    C- for the course...

--- typescipt/typescript-declaration-file/app/Communicator.ts
+++ typescipt/typescript-declaration-file/app/Communicator.ts
@@ -1,12 +1,5 @@
 import _ from "lodash";
 
-class Communicator {
-  constructor() {}
+const greeting = greet("Joshua");
 
-  greet(message: string) {
-    return "<h1>" + _.toUpper(message) + "</h1>";
-  }
-}
-
-var communicator = new Communicator();
-document.body.innerHTML = communicator.greet("Hello, world!");
+document.body.innerHTML = _.upperCase(greeting);
